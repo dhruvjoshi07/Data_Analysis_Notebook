209dd425d8e7633520cad95d809fa861c7f95f71
dhruvjoshi07 <1002dhruvj@gmail.com>
2026-03-27 18:20:32 +0530

    commit

---
+++ python/second.py
@@ -0,0 +1,6 @@
+num1=int(input("Enter the number:"))
+num2=int(input("Enter the second number:"))
+if num1>num2:
+    print(f"number is {num1} greater than {num2}")
+else:
+    print(f"second number is {num2} greater than {num1}")
