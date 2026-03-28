0cec44b88387373978f3d47a3901e464c0d57bf3
dhruvjoshi07 <1002dhruvj@gmail.com>
2026-03-28 15:08:18 +0530

    commit

--- python/third.py
+++ python/third.py
@@ -13,4 +13,43 @@ elif temp>=30 and temp<40:
     print(f"{name} its fucking Hot at {temp} degree celsius....")
 else:
     print(f"{name} its very hot at {temp} degree celsius....")
+# repeatation
+name=str(input("Enter your name : "))
+n=int(input(f"{name}! Enter the times you want to print hello {name}! "))
+for i in range(n):
+    print(f"hello {name}")
+# natural number
+m=int(input(f"{name}! Enter the end of natural number: "))
+for i in range(1,m+1):
+    print(i)
+#rev
+a=int(input(f"{name}! Enter the number to reverse"))
+for i in range(a,0,-1):
+    print(i)
+# table
+num=int(input("Enter the number: "))
+for i in range(num,num*10+1,num):
+    print(f"{num}*{i//num}={i}")
+#sum upto n terms
+sum=0
+for i in range(0,num+1):
+    sum=sum+i
+
+print(sum)
+# factorial
+
+# sum of all even and odd numbers in range seaprately
+num = int(input("Enter the number: "))
+
+even_sum = 0
+odd_sum = 0
+
+for i in range(1, num+1):
+    if i % 2 == 0:
+        even_sum += i
+    else:
+        odd_sum += i
+
+print("Sum of even numbers:", even_sum)
+print("Sum of odd numbers:", odd_sum)
 
