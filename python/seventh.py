af24083be59617a0a0845d682d39a9874c2b325d
dhruvjoshi07 <1002dhruvj@gmail.com>
2026-03-31 15:35:42 +0530

    commit

---
+++ python/seventh.py
@@ -0,0 +1,39 @@
+#write a py script to merege 2 py dictionaries
+a={
+    1:"hello",
+    "a":23,
+    3:{2,3,1,4,4,2},
+    4:[32,44,12,56,33],
+    5:{1,2,3,4,5}
+}
+b={
+    6:3.12,
+    7:{1},
+    8:{2,},
+    9:"D+3"
+}
+a.update(b)
+print(a)
+
+for i in b:
+    a[i]=b[i]
+    print(a)
+
+#write a py program to sum all the values in a dictionary
+sum=0
+for i in a:
+    sum=sum + a[i]
+
+print(sum)
+
+#count the frequency of each elements in a list
+a=[1,1,1,2,2,2,3,3,3,4,4,4,5,5,5,5]
+d={}
+for i in a:
+    if i in d.keys():
+        d[i]+=1
+    else:
+        d[i]=1
+print(d)
+
+#write a python program to combine 2 dictionary by adding values for common keys
